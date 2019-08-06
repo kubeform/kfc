1ecc7c2631560f4210e180f2e9de9de91c2ca686
Fahim Abrar <fahimabrar@appscode.com>
2019-08-06 12:28:50 +0600

    Generate TFState file from output

--- controllers/terraform.go
+++ controllers/terraform.go
@@ -9,6 +9,7 @@ func terraformInit(resPath string) error {
 	cmd := exec.Command("terraform", "init")
 	cmd.Dir = resPath
 	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	err := cmd.Run()
 	if err != nil {
 		return err
@@ -21,6 +22,7 @@ func terraformApply(resPath string) error {
 	cmd := exec.Command("terraform", "apply", "-auto-approve")
 	cmd.Dir = resPath
 	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	err := cmd.Run()
 	if err != nil {
 		return err
@@ -33,6 +35,7 @@ func terraformDestroy(resPath string) error {
 	cmd := exec.Command("terraform", "destroy", "-auto-approve")
 	cmd.Dir = resPath
 	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	err := cmd.Run()
 	if err != nil {
 		return err
